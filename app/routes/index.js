4e945163406cbd8ca3df3c9796021f7d00790f41
antoniolopesjr <antonio.lopesjr@outlook.com>
2020-02-27 17:35:13 -0300

    movie-list

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,28 +1,30 @@
 import Route from '@ember/routing/route';
 
-const COMMUNITY_CATEGORIES = [
+/*const COMMUNITY_CATEGORIES = [
     'Condo',
     'Townhouse',
     'Apartment'
-  ];
+  ];*/
+
 
 export default class IndexRoute extends Route {
   async model() {
-    let response = await fetch('/api/rentals.json');
+    let response = await fetch('/api/upcoming.json');
 
-    let { data } = await response.json();
+    let { results } = await response.json();
 
-    return data.map(model => {
-      let { attributes } = model;
-      let type;
+    return results.map(model => {
+      let results  = model;
+    
+      /*let type;
 
       if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
         type = 'Community';
       } else {
         type = 'Standalone';
-      }
+      }*/
 
-      return { type, ...attributes };
+      return { ...results };
     });
 }
 }
