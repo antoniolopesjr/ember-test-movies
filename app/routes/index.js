69cc5e88c86480c8cfbaef60bec1c4236d70489a
antoniolopesjr <antonio.lopesjr@outlook.com>
2020-02-28 11:34:40 -0300

    ok-now

--- app/routes/index.js
+++ app/routes/index.js
@@ -9,7 +9,7 @@ import Route from '@ember/routing/route';
 
 export default class IndexRoute extends Route {
   async model() {
-    let response = await fetch('/api/upcoming.json');
+    let response = await fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=c5850ed73901b8d268d0898a8a9d8bff&language=en-US&page=1');
 
     let { results } = await response.json();
 
