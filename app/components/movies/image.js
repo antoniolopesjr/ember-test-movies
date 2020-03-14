161f1dbf42db4480516fa7a9a7a48ae047f08a45
Antonio Lopes <antonio.lopesjr@outlook.com>
2020-03-13 21:02:22 -0300

    Adjustment

--- app/components/movies/image.js
+++ app/components/movies/image.js
@@ -3,9 +3,9 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class MoviesImageComponent extends Component {
-  @tracked isLarge = false;
+  /*@tracked isLarge = false;
 
   @action toggleSize() {
     this.isLarge = !this.isLarge;
-  }
+  }*/
 }
