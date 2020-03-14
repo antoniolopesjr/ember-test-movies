27f9d97a93e2ab172638649262be39ae6060e4b1
Antonio Lopes <antonio.lopesjr@outlook.com>
2020-03-14 00:36:05 -0300

    Genre add

--- app/routes/movie.js
+++ app/routes/movie.js
@@ -6,11 +6,20 @@ export default class MovieRoute extends Route {
     let modelStringify = JSON.stringify(receiveModel);
     let movie_id_OK = modelStringify.replace(/[\\"]/g, '');
 
-    let jsonString = 'https://api.themoviedb.org/3/movie/' +movie_id_OK + '?api_key=c5850ed73901b8d268d0898a8a9d8bff&language=en-US';
+    let jsonString = 'https://api.themoviedb.org/3/movie/' + movie_id_OK + '?api_key=c5850ed73901b8d268d0898a8a9d8bff&language=en-US';
 
     let response = await fetch(jsonString);
     let results = await response.json();
 
-    return { ...results };
+    //let jsonStringlike = JSON.stringify(results);
+
+    //let data = results.genres.get('firstObject');
+    let data = results.genres[0];
+    
+    window.console.log(data.name);
+
+    return {
+      data, ...results
+    };
   }
 }
