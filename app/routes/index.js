623e9caf40c6319d56e4428965706fa4eba898b2
Antonio Lopes <antonio.lopesjr@outlook.com>
2020-03-16 19:57:18 -0300

    Code cleanup

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,9 +3,7 @@ import Route from '@ember/routing/route';
 export default class IndexRoute extends Route {
   async model() {
     let response = await fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=c5850ed73901b8d268d0898a8a9d8bff&language=en-US&page=1');
-    let {
-      results
-    } = await response.json();
+    let { results } = await response.json();
 
     return results.map(model => {
       let results = model;
--- app/routes/movie.js
+++ app/routes/movie.js
@@ -1,4 +1,5 @@
 import Route from '@ember/routing/route';
+
 export default class MovieRoute extends Route {
 
   async model(params) {
