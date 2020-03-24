ecbd2783bbd8c6734c43c451ec6d835a70dc7744
Antonio Lopes <antonio.lopesjr@outlook.com>
2020-03-24 10:15:33 -0300

    update

---
+++ app/modifiers/fade-in-down.js
@@ -0,0 +1,20 @@
+import Modifier from 'ember-modifier';
+
+export default class FadeDownModifier extends Modifier {
+    didReceiveArguments() {
+        const { duration, delay } = this.args.named;
+
+        this.element.animate(
+            [
+                { opacity: 0, transform: 'translateY(-20px)' },
+                { opacity: 1, transform: 'translateY(0px)' }
+            ],
+            {
+                duration: duration || 1500,
+                delay: delay || 0,
+                /*easing: 'cubic-bezier(0.075, 0.82, 0.165, 1)',
+                fill: 'backwards'*/
+            }
+        );
+    }
+}
---
+++ tests/integration/modifiers/fade-in-down-test.js
@@ -0,0 +1,15 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Modifier | fade-in-down', function(hooks) {
+  setupRenderingTest(hooks);
+
+  // Replace this with your real tests.
+  test('it renders', async function(assert) {
+    await render(hbs`<div {{fade-in-down}}></div>`);
+
+    assert.ok(true);
+  });
+});
