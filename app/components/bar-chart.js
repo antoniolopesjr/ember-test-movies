cf6d485c8bf93c1db1a696e34559d42dc76edbc6
Antonio Lopes <antonio.lopesjr@outlook.com>
2020-07-09 15:48:05 -0300

    Update

---
+++ app/components/bar-chart.js
@@ -0,0 +1,19 @@
+import Component from '@glimmer/component';
+import { select } from 'd3-selection';
+
+
+export default class BarChartComponent extends Component {
+    init(){
+        let svg = select(this.$('svg')[0])
+        //let svg = this.element.querySelector('svg')[0];
+        
+        svg.append('rect')
+        .attr('width', 20)
+        .attr('height', 100);
+
+        svg.append('rect')
+        .attr('width', 20)
+        .attr('height', 50)
+        .attr('x', 30);
+    }
+}
